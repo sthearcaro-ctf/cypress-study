5e1a363b4b26c10f9bff584a7a441b8de0a683c5
sthearcaro-ctf <sthefani.carvalho@cotefacil.com>
2025-02-26 10:23:52 -0300

    teste de documentacao em relatorio com uso do mochawesome

--- cypress.config.js
+++ cypress.config.js
@@ -6,5 +6,14 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
     video: true,
+    reporter: 'mochawesome',
+    // configuração do mochawesome (relatórios documentados)
+    reporterOptions: {
+      reportDir: 'cypress/results', // caminho da pasta
+      overwrite: false, // se ele vai ou não sobrescrever
+      html: true, // gera arquivo html
+      json: false, // gera arquivo em json
+      timestamp: "mmddyyyy_HHMMss" // tipo de data (mes/dia/ano_h/min/seg)
+    }
   },
 });
