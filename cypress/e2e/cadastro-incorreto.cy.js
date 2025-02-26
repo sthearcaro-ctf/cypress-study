7fd5cd708ac0f5b04a3868961f21af09e588cdce
sthearcaro-ctf <sthefani.carvalho@cotefacil.com>
2025-02-26 09:52:17 -0300

    teste usando o terminal -npx cypress run- e documentacao em video

--- cypress/e2e/cadastro-incorreto.cy.js
+++ cypress/e2e/cadastro-incorreto.cy.js
@@ -1,7 +1,10 @@
 describe('verificação da página inicial do cypress', () => {
-    it('preencher os campos do formulario incorretamente e exibir mensagem ao usuario', () => {
+    beforeEach(() => {
         cy.visit('https://adopet-frontend-cypress.vercel.app/');
         cy.get('[data-test="register-button"]').click({ force: true });
+    })
+
+    it('preencher os campos do formulario incorretamente e exibir mensagem ao usuario', () => {
         cy.get('[data-test="submit-button"]').click();
         cy.contains('É necessário informar um endereço de email').should('be.visible');
         cy.contains('Crie uma senha').should('be.visible');
